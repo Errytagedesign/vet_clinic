b484897b0980b062b3d3665889833d32883885b1
Errytagedesign <errytage@gmail.com>
2023-09-23 11:50:32 +0100

    Add muiltple tables

--- schema.sql
+++ schema.sql
@@ -1,16 +1,33 @@
--- Active: 1695416737359@@127.0.0.1@5432@vet_clinic
-/* Database schema to keep the structure of entire database. */
+-- vetclinic database
 
-CREATE TABLE animals (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    date_of_birth DATE,
-    escape_attempts INT,
-    neutered BOOLEAN,
-    weight_kg DECIMAL(10, 2)
+CREATE DATABASE vet_clinic;
+
+-- animals table
+
+CREATE TABLE animals ( id SERIAL PRIMARY KEY,
+                                         name VARCHAR(255),
+                                              date_of_birth DATE, escape_attempts INTEGER, neutered BOOLEAN, weight_kg DECIMAL, species VARCHAR(255), -- 'species column'
+ species_id INTEGER, -- 'species_id column'
+ owner_id INTEGER -- 'owner_id column'
 );
 
--- Modify your schema.sql file.
---Add a column species of type string to your animals table.
-ALTER TABLE animals
-ADD COLUMN species VARCHAR(255)
+-- foreign key
+
+ALTER TABLE animals ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id) REFERENCES species(id);
+
+-- foreign key
+
+ALTER TABLE animals ADD CONSTRAINT fk_owner
+FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+-- owner's table
+
+CREATE TABLE owners ( id SERIAL PRIMARY KEY,
+                                        full_name VARCHAR(100),
+                                                  age INTEGER);
+
+-- species table
+
+CREATE TABLE species ( id SERIAL PRIMARY KEY,
+                                         name VARCHAR(100));
