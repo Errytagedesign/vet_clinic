ea598748172535eada6361b1f78c65b2a773615d
Errytagedesign <errytage@gmail.com>
2023-09-23 12:31:26 +0100

    Add remove species from animals tables

--- schema.sql
+++ schema.sql
@@ -4,12 +4,16 @@ CREATE DATABASE vet_clinic;
 
 -- animals table
 
-CREATE TABLE animals ( id SERIAL PRIMARY KEY,
-                                         name VARCHAR(255),
-                                              date_of_birth DATE, escape_attempts INTEGER, neutered BOOLEAN, weight_kg DECIMAL, species VARCHAR(255), -- 'species column'
+CREATE TABLE animals (id SERIAL PRIMARY KEY,
+                                        name VARCHAR(255),
+                                             date_of_birth DATE, escape_attempts INTEGER, neutered BOOLEAN, weight_kg DECIMAL, species VARCHAR(255), -- 'species column'
  species_id INTEGER, -- 'species_id column'
  owner_id INTEGER -- 'owner_id column'
 );
+
+
+ALTER TABLE animals
+DROP column species;
 
 -- foreign key
 
@@ -23,11 +27,11 @@ FOREIGN KEY (owner_id) REFERENCES owners(id);
 
 -- owner's table
 
-CREATE TABLE owners ( id SERIAL PRIMARY KEY,
-                                        full_name VARCHAR(100),
-                                                  age INTEGER);
+CREATE TABLE owners (id SERIAL PRIMARY KEY,
+                                       full_name VARCHAR(100),
+                                                 age INTEGER);
 
 -- species table
 
-CREATE TABLE species ( id SERIAL PRIMARY KEY,
-                                         name VARCHAR(100));
+CREATE TABLE species (id SERIAL PRIMARY KEY,
+                                        name VARCHAR(100));
